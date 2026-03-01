7d136a3f03937dc6a1ec0a095e9a5de61f4a7758
Mosudi I. O. <imosudi@gmail.com>
2026-03-01 14:57:46 +0100

    BLE Engine

--- ble_engine/modules/utility.py
+++ ble_engine/modules/utility.py
@@ -3,9 +3,9 @@
 def value_change_handler(iface, prop_changed, prop_removed):
     if 'Value' in prop_changed:
         print(f"Value: {prop_changed['Value']}")
-    if 'Value' in prop_removed:
+    """if 'Value' in prop_removed:
         print("Value removed")
     if 'Value' not in prop_changed and 'Value' not in prop_removed:
-        print("Value unchanged")
+        print("Value unchanged")"""
     
         
