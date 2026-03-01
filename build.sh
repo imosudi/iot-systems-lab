cc6a568079fbecfe4f86565e39c2d1a27c0a6229
Mosudi I. O. <imosudi@gmail.com>
2026-03-01 14:56:19 +0100

    BLE Engine

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
-
 # build.sh
+
+
+cd ble_engine/
 
 podman build . -t imosudi/ble_engine
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-
 # start.sh
+
 podman run --userns=keep-id -v \
     /run/dbus:/run/dbus:z \
     -it localhost/imosudi/ble_engine:latest 
